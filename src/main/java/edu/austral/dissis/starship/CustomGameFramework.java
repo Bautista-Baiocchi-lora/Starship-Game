dd3122689e1cf842415d221c9f558ff8579b3566
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-11-13 16:14:23 -0300

    clean out

--- src/main/java/edu/austral/dissis/starship/CustomGameFramework.java
+++ src/main/java/edu/austral/dissis/starship/CustomGameFramework.java
@@ -19,57 +19,17 @@ import static java.util.Arrays.asList;
 
 public class CustomGameFramework implements GameFramework {
 
-    private StarshipDrawer starshipDrawer;
-    private Starship starship1 = new Starship(vector(200, 200), vector(0, -1));
-    private Starship starship2 = new Starship(vector(400, 400), vector(0, -1));
-
-    private final CollisionEngine engine = new CollisionEngine();
 
     @Override
     public void setup(WindowSettings windowsSettings, ImageLoader imageLoader) {
-        windowsSettings
-            .setSize(500, 500);
+        windowsSettings.setSize(500, 500);
 
-        starshipDrawer = new StarshipDrawer(imageLoader.load("spaceship.png"));
+
     }
 
     @Override
     public void draw(PGraphics graphics, float timeSinceLastDraw, Set<Integer> keySet) {
-        updateStarship(keySet);
-        drawStarships(graphics);
-        checkCollisions();
-    }
 
-    private void checkCollisions() {
-        final List<SquareCollisionable> collisionables = asList(
-                starshipDrawer.getCollisionable(starship1),
-                starshipDrawer.getCollisionable(starship2)
-        );
-
-        engine.checkCollisions(collisionables);
-    }
-
-    private void drawStarships(PGraphics graphics) {
-        starshipDrawer.draw(graphics, starship1);
-        starshipDrawer.draw(graphics, starship2);
-    }
-
-    private void updateStarship(Set<Integer> keySet) {
-        if (keySet.contains(PConstants.UP)) {
-            starship1 = starship1.moveForward(2);
-        }
-
-        if (keySet.contains(PConstants.DOWN)) {
-            starship1 = starship1.moveBackwards(2);
-        }
-
-        if (keySet.contains(PConstants.LEFT)) {
-            starship1 = starship1.rotate(-1 * PConstants.PI / 60);
-        }
-
-        if (keySet.contains(PConstants.RIGHT)) {
-            starship1 = starship1.rotate(PConstants.PI / 60);
-        }
     }
 
     @Override
